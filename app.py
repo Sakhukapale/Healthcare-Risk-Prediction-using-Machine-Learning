91a1a08ae765a62afa58f5d395c7644dafb592f0
Kapale Sakhubai <sakhukapale27@gmail.com>
2026-03-27 09:20:40 +0530

    Refactor input fields and prediction button in app.py

--- app.py
+++ app.py
@@ -6,18 +6,30 @@ model = pickle.load(open("heart_model.pkl", "rb"))
 
 st.title("Heart Disease Prediction")
 
+# All 13 inputs
 age = st.number_input("Age")
+sex = st.selectbox("Sex", [0,1])
+cp = st.selectbox("Chest Pain", [0,1,2,3])
+trestbps = st.number_input("BP")
 chol = st.number_input("Cholesterol")
-bp = st.number_input("Blood Pressure")
-thalach = st.number_input("Max Heart Rate")
+fbs = st.selectbox("FBS >120", [0,1])
+restecg = st.selectbox("Rest ECG", [0,1,2])
+thalach = st.number_input("Max HR")
+exang = st.selectbox("Exercise Angina", [0,1])
+oldpeak = st.number_input("Oldpeak")
+slope = st.selectbox("Slope", [0,1,2])
+ca = st.selectbox("CA", [0,1,2,3])
+thal = st.selectbox("Thal", [0,1,2,3])
 
-if st.button("Predict Risk"):
+if st.button("Predict"):
 
-    data = np.array([[age, chol, bp, thalach]])
+    data = np.array([[age, sex, cp, trestbps, chol, fbs,
+                      restecg, thalach, exang, oldpeak,
+                      slope, ca, thal]])
 
     prediction = model.predict(data)
 
     if prediction[0] == 1:
-        st.error("High Risk of Heart Disease")
+        st.error("High Risk")
     else:
-        st.success("Low Risk of Heart Disease")
+        st.success("Low Risk")
